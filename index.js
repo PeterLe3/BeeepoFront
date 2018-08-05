648443b770c6c7b32230ba7fba4cb709c7a6458a
Peter Le <Peterle3@csus.edu>
2018-08-05 02:03:18 -0700

    added streamer pize page

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
 	$('#text').hide();
 	$('#beepo').hide();
-	$('#beepo').delay(3000).fadeIn(2000);
+	$('#beepo').delay(3000).fadeIn(1000);
 	$('#beepo').delay(8000).fadeOut(1500);
- 	$('#text').delay(13000).fadeOut(1500);
+ 	$('#text').delay(12000).fadeOut(1500);
 
 
 
@@ -13,26 +13,26 @@ $( document ).ready(function() {
 
 
 	 })
-	$("#enterSong").submit(function(event) { 
-		var song = $('#text').val();
-		var userID = 1;
-		$.ajax({
-			method: "POST",
-			url: "marksurl",
-			data : {
+	// $("#enterSong").submit(function(event) { 
+	// 	var song = $('#text').val();
+	// 	var userID = 1;
+	// 	$.ajax({
+	// 		method: "POST",
+	// 		url: "marksurl",
+	// 		data : {
 
-				song : song,
-				userID: userID
-			},
-			success: function() {
-				alert("Sucessfully sent song guess from user to server!");
-			}
-			error: function() {
-				alert("Something wrong!");
-			}
+	// 			song : song,
+	// 			userID: userID
+	// 		},
+	// 		success: function() {
+	// 			alert("Sucessfully sent song guess from user to server!");
+	// 		}
+	// 		error: function() {
+	// 			alert("Something wrong!");
+	// 		}
 
-		})
-	})
+	// 	})
+	// })
 
 
 	
