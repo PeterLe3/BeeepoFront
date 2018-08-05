0cc8a4656863987c26856f12341aeafd2543e0db
Andy Le <leal1@uci.edu>
2018-08-04 21:10:49 -0700

    mod table

--- scoreboard/index.js
+++ scoreboard/index.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
 
    // jQuery methods go here..
-
-   var td =  "<tr> <td> user </td>" +
-   			"<td> score </td>" +
-   			"</tr";
-   for( var i = 0; i < 10; i++)
+   function addTable() 
    {
-   		$("#table").append(td);
-   }
+	   var td =  "<tr> <td> user </td>" +
+	   			"<td> score </td>" +
+	   			"</tr";
+	   for( var i = 0; i < 10; i++)
+	   {
+	   		$("#table").append(td);
+	   }
    
+   }
+   	$.ajax({
+   		url: "dummy.json", 
+   		type: "GET",
+   		dataType: "json"
+   		success: function(result){
+     
+        }});
 });
