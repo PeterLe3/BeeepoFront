c419bc809f9b0c1b2b226e7ae0b560d3f94cae70
Peter Le <Peterle3@csus.edu>
2018-08-04 23:43:04 -0700

    beepo animations, ajax song submission

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 $( document ).ready(function() {
+	$('#text').hide();
 	$('#beepo').hide();
 	$('#beepo').delay(3000).fadeIn(2000);
 	$('#beepo').delay(8000).fadeOut(1500);
+ 	$('#text').delay(13000).fadeOut(1500);
+
+
+
+
+	$('#beepo').click(function() {
+		$('#text').show();
+
+
+	 })
+	$("#enterSong").submit(function(event) { 
+		var song = $('#text').val();
+		var userID = 1;
+		$.ajax({
+			method: "POST",
+			url: "marksurl",
+			data : {
+
+				song : song,
+				userID: userID
+			},
+			success: function() {
+				alert("Sucessfully sent song guess from user to server!");
+			}
+			error: function() {
+				alert("Something wrong!");
+			}
+
+		})
+	})
 
 
 	
