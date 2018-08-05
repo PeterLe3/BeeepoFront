e462693c430ee87214ce21f971d8d6670da86723
Andy Le <leal1@uci.edu>
2018-08-05 05:17:33 -0700

    login page

---
+++ login_page/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	alert(window.location.href)
+	$("#submit").click(function(e){
+		var username = $("#username").val();
+		var password = $("#password").val();
+		alert(username);
+		alert(password);
+		e.preventDefault();
+		alert(window.location.href + "../test.json");
+		$.ajax({
+			method: "POST",
+			url: window.location.href + "../test.json",
+			
+			data: {
+				username: username,
+				password: password
+			},
+			sucess:function(){
+				alert("Sucessfully sent");
+			},
+			error:function(){
+				alert("ERROR");
+			}
+		})
+		//alert(window.location.href)
+	})
+});
