7117ca2c2303fec16346d95de03260575abb5b60
Andy Le <leal1@uci.edu>
2018-08-04 20:50:25 -0700

    adding scoreboard

---
+++ scoreboard/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+   // jQuery methods go here..
+
+   var td =  "<tr> <td> user </td>" +
+   			"<td> score </td>" +
+   			"</tr";
+   for( var i = 0; i < 10; i++)
+   {
+   		$("#table").append(td);
+   }
+   
+});
